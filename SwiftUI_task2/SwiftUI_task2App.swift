91750e4209c55a9a3643a93398f613be1cdf059c
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-12-03 14:27:58 +0300

    Initial Commit

---
+++ SwiftUI_task2/SwiftUI_task2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_task2App.swift
+//  SwiftUI_task2
+//
+//  Created by Evgenii Mikhailov on 03.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_task2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
